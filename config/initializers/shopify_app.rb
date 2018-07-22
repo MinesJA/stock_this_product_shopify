c35b0b72adf7115acebbca0d0d6bb983f65dcf5a
Jonathan Mines <minesja@gmail.com>
2018-07-22 17:34:56 -0400

    changed api keys

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "ad0c1238babf1bd9f03d95ef82b12cd6"
-  config.secret = "4bc20b714bc72282c15a0b43f8113f65"
+  config.secret = "01c85a228869f21cd0a4ec6aa775de70"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
